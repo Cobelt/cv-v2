86ddcb5659059f91ff080b8780641b25435d1893
Cobelt <paul_emile@hotmail.fr>
2024-08-04 01:37:43 +0200

    feat: setup deps, config files, and public assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -13,8 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        archivo: "Archivo",
+        latoLight: "LatoLight",
+        latoBold: "LatoBold",
+        latoReg: "LatoReg",
+        jostLight: "JostLight",
+        jostBold: "JostBold",
+        jostReg: "JostReg",
+        rubikLight: "RubikLight",
+        rubikBold: "RubikBold",
+        rubikReg: "RubikReg",
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
