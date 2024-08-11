3d24dc4c0c9017af12a94b6cd429ec78cfc53903
Cobelt <paul_emile@hotmail.fr>
2024-08-11 15:58:17 +0200

    feat: idk what to do on this page :((

--- src/pages/stack.tsx
+++ src/pages/stack.tsx
@@ -29,27 +29,44 @@ export default function Stack({ previousRoute }: IPageProps) {
         </PageTitle>
 
         <section className="area-[know-how]">
-          <h2 className="font-jostBold text-2xl">Mes savoirs faire</h2>
+          <h2 className="font-jostBold text-2xl">Concrètement</h2>
 
           <h4>Je sais ...</h4>
-          <ul className="font-latoBold text-xl">
-            <li className="text-xl">Développer un site vitrine</li>
-            <li className="text-xl">Développer un site marchand</li>
-            <li className="text-xl">Développer un backoffice</li>
-            <li className="text-xl">Développer une app interne</li>
-            <li className="text-xl">Développer une app mobile</li>
-            <li className="text-xl">Maintenir et faire évoluer un projet</li>
-            <li className="text-xl">
-              Maitriser la couche métier d{"'"}un projet existant
-            </li>
+          <ul className="font-latoBold text-lg">
+            <li>Développer un site vitrine</li>
+            <li>Développer un site marchand</li>
+            <li>Développer un backoffice</li>
+            <li>Développer une app interne</li>
+            <li>Développer une app mobile</li>
+            <li>Maintenir et faire évoluer un projet</li>
+            <li>Maitriser la couche métier d{"'"}un projet existant</li>
           </ul>
         </section>
         <section className="area-[techs]">
-          <h2 className="font-jostBold text-2xl">Mes compétences techniques</h2>
+          <h2 className="font-jostBold text-2xl">Mes technos</h2>
 
-          <ul className="font-latoBold text-xl">
-            <li className="text-xl">React</li>
-            <li className="text-xl">...</li>
+          <ul className="font-latoBold text-lg">
+            <h4 className="text-xl">Languages</h4>
+            <li>JavaScript/TypeScript/EcmaScript 6+</li>
+            <li>PHP</li>
+            <li>Ruby</li>
+            <li>C++</li>
+            <li></li>
+            <h4 className="text-xl">Framework Front</h4>
+            <li>React</li>
+            <li>Next</li>
+            <li>Svelte</li>
+            <li>Vue 2</li>
+            <h4 className="text-xl">Framework Back</h4>
+            <li>Symfony</li>
+            <li>Ruby-On-Rails</li>
+            <li>Silex</li>
+            <li>Sharepoint</li>
+            <h4 className="text-xl">Willing to learn</h4>
+            <li>Cypress</li>
+            <li>Dart + Flutter</li>
+            <li>Svelte (more)</li>
+            <li>Vue (more)</li>
           </ul>
         </section>
 
@@ -57,16 +74,16 @@ export default function Stack({ previousRoute }: IPageProps) {
           <h2 className="font-jostBold text-2xl">Mes savoirs être</h2>
 
           <h4>Je sais ...</h4>
-          <ul className="font-latoBold text-xl">
-            <li className="text-xl">Travailler en équipe de tout taille</li>
-            <li className="text-xl">Communiquer avec mes collègues</li>
-            <li className="text-xl">M{"'"}adapter au besoin</li>
-            <li className="text-xl">Prendre du recul</li>
-            <li className="text-xl">Être créatif et inventif</li>
-            <li className="text-xl">Être rigoureux et précis</li>
-            <li className="text-xl">Être force de proposition</li>
-            <li className="text-xl">Être versatile</li>
-            <li className="text-xl">Être autonome</li>
+          <ul className="font-latoBold text-lg">
+            <li>Travailler en équipe de tout taille</li>
+            <li>Communiquer avec mes collègues</li>
+            <li>M{"'"}adapter au besoin</li>
+            <li>Prendre du recul</li>
+            <li>Être créatif et inventif</li>
+            <li>Être rigoureux et précis</li>
+            <li>Être force de proposition</li>
+            <li>Être versatile</li>
+            <li>Être autonome</li>
           </ul>
         </section>
       </main>
