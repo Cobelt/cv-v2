3e7c65f15329654fdc138a2d92340ca8be786287
Cobelt <paul_emile@hotmail.fr>
2024-08-13 03:42:40 +0200

    feat: next/head in pageTransition + moved padding inside too so I dont have to set it anymore on each page

--- src/components/PageTransition/PageTransition.tsx
+++ src/components/PageTransition/PageTransition.tsx
@@ -1,23 +1,45 @@
 import { ReactNode, useEffect, useRef, useState } from "react"
 import { motion as m } from "framer-motion"
-import { cN } from "@/lib"
+import { SITE_URL, cN } from "@/lib"
 import useAnimatePageProps from "@/hooks/useAnimatePageProps"
+import Head from "next/head"
 
 interface IPageTransition {
   children: ReactNode
   previousRoute: string
+  headTitle?: string
   className?: string
+  paddingTop?: string
+  paddingBottom?: string
+  paddingHorizontal?: string
 }
 
 export default function PageTransition({
   children,
   className,
+  headTitle,
+  paddingTop = "pt-20 lg:pt-52",
+  paddingBottom = "pb-32 lg:pb-16",
+  paddingHorizontal = "2xl:px-[3vw] 3xl:px-[8vw]",
   previousRoute,
 }: IPageTransition) {
   const animation = useAnimatePageProps(previousRoute)
 
   return (
-    <m.div className={cN("absolute inset-0", className)} {...animation}>
+    <m.div
+      className={cN(
+        "absolute inset-0",
+        paddingTop,
+        paddingBottom,
+        paddingHorizontal,
+        className
+      )}
+      {...animation}
+    >
+      <Head>
+        <title>{headTitle ? `${headTitle} - ${SITE_URL}` : SITE_URL}</title>
+      </Head>
+
       {children}
     </m.div>
   )
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,7 @@
 export const SITE_NAME = "polemil.dev"
 export const SITE_DESCRIPTION = "Paul-Emile Moreau's online CV/Resume"
-export const SITE_URL = "https://polemil.dev"
+export const DOMAIN = "polemil.dev"
+export const SITE_URL = "https://" + DOMAIN
 export const AUTHOR = "Paul-Emile Moreau"
 export const AUTHOR_URL = "https://polemil.dev"
 export const API_URL = "https://bo.polemil.dev/api"
