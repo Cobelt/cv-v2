2440ce763b1974415d676f6aca8bf101e7ee4187
Cobelt <paul_emile@hotmail.fr>
2024-08-18 04:45:37 +0200

    feat: picture animation rework

--- src/features/Profile/Picture/Picture.tsx
+++ src/features/Profile/Picture/Picture.tsx
@@ -1,12 +1,14 @@
 import { motion as m } from "framer-motion"
-import { useTranslation } from "react-i18next"
 import Image from "next/image"
+import { useTranslation } from "react-i18next"
 
+import pictureOfMe from "@/../public/picture.jpg"
+import pictureWithoutBg from "@/../public/pictureWithoutBg.png"
 import { fadeInItem } from "@/animations/pageContainer"
-import pictureOfMe from "@/../public/me.jpg"
-import ZigZagArrowSvg from "../../../components/icons/ZigZagArrow"
-import { WithClassNameProps } from "@/types"
+import ZigZagArrowSvg from "@/components/icons/ZigZagArrow"
 import { cN } from "@/lib"
+import { WithClassNameProps } from "@/types"
+import css from "./Picture.module.scss"
 
 export default function Picture({ className }: WithClassNameProps) {
   const [t] = useTranslation()
@@ -15,30 +17,63 @@ export default function Picture({ className }: WithClassNameProps) {
     <m.div
       variants={fadeInItem}
       className={cN(
-        className,
-        "flex sm:items-center sm:justify-end lg:justify-start",
-        "group mt-auto sm:mb-auto relative"
+        "flex items-end md:items-center sm:justify-end",
+        "mt-auto lg:mb-auto relative font-rubikBold ",
+        className
       )}
     >
-      <Image
-        className="flex-1 lg:flex-[initial] hover:scale-105 border-blue-500 border-8 rounded-full md:overflow-hidden aspect-square object-cover transition-transform"
-        src={pictureOfMe}
-        alt={t("profile.photo.floki")}
-        style={{
-          width: "calc(3em + 12vw)",
-          maxWidth: "14rem",
-          minHeight: "8.5rem",
-          minWidth: "8.5rem",
-        }}
-      />
-      <div className="hidden lg:flex group-hover:opacity-100 opacity-0 transition-opacity flex-col-reverse absolute -top-16 left-0 2xl:left-2/3 z-10 w-auto text-nowrap text-lg font-rubikBold">
+      <div className="relative group">
+        <Image
+          className={cN(
+            css.picture,
+            "min-w-28 min-h-28 blur-0 group-hover:blur",
+            "lg:flex-[initial] duration-300"
+          )}
+          style={{ width: "var(--picture-clamp)" }}
+          src={pictureOfMe}
+          alt={t("profile.photo.floki")}
+        />
+
+        <div
+          className="inset-0 absolute border-4 border-stone-50 border-dashed rounded-full z-[5] rotate-[-20deg] group-hover:rotate-12"
+          style={{ transition: "all 1s, border-top-color 0.3s" }}
+        />
+        <div
+          className="inset-0 absolute border-4 border-stone-50 group-hover:border-t-[transparent] border-dashed rounded-full z-[7] rotate-[-20deg] group-hover:rotate-12"
+          style={{ transition: "all 1s, border-top-color 0.3s" }}
+        />
+
+        <Image
+          className={cN(
+            css.picture,
+            css.noBg,
+            "min-w-28 min-h-28 z-[4] group-hover:scale-110 origin-bottom group-hover:z-[6]",
+            "lg:flex-[initial]"
+          )}
+          style={{ width: "var(--picture-clamp)" }}
+          src={pictureWithoutBg}
+          alt={t("profile.photo.floki")}
+        />
+      </div>
+
+      <div
+        className={cN(
+          "hidden lg:flex flex-col-reverse group-hover:fade-in z-10 w-auto text-nowrap text-lg",
+          "absolute -top-16 left-0 2xl:left-2/3"
+        )}
+      >
         <ZigZagArrowSvg
           className="ml-10 2xl:ml-0 -rotate-[135deg] 2xl:-rotate-45"
           size={30}
         />
         <span>{t("profile.photo.me")}</span>
       </div>
-      <div className="hidden lg:flex group-hover:opacity-100 opacity-0 transition-opacity flex-col absolute -bottom-16 left-0 2xl:left-2/3 z-10 w-auto text-nowrap text-lg font-rubikBold">
+      <div
+        className={cN(
+          "hidden lg:flex flex-col group-hover:fade-in z-10 w-auto text-nowrap text-lg",
+          "absolute -bottom-16 left-0 2xl:left-2/3"
+        )}
+      >
         <ZigZagArrowSvg
           className="ml-10 2xl:ml-0 rotate-[135deg] 2xl:rotate-45"
           size={30}
