f65646f77d63d9da6ee797e102dc2875a9bc85af
Cobelt <paul_emile@hotmail.fr>
2024-08-19 06:30:18 +0200

    fix: better Logo anim in mobile

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,32 +1,25 @@
 import { cN } from "@/lib"
 import { WithClassNameProps } from "@/types"
 import Link from "next/link"
-import { CSSProperties } from "react"
 
 export default function Logo({ className }: WithClassNameProps) {
   return (
     <Link href="/">
       <h2
         className={cN(
-          "group flex text-3xl md:text-5xl font-bukhari min-w-20 underline-rounded",
+          "group flex text-3xl md:text-5xl font-bukhari min-w-20",
+          "underline-rounded underline-stone-50",
           className
         )}
-        style={
-          {
-            "--underline-color": "#fff",
-          } as CSSProperties
-        }
       >
-        <div className="mr-3">Polémil</div>
+        <div className="mr-2 md:mr-3">Polémil</div>
         <div className="bounce-in relative">
           <span
             className={cN(
-              "w-2 h-2 bg-stone-50 rounded-full inline-block origin-[-0.55rem_bottom] xl:origin-[-0.45rem_bottom] opacity-100 [transition:transform_0s,opacity_0.3s_0.55s]",
-              "group-hover:rotate-[134deg] group-hover:opacity-0 group-hover:[transition:transform_0.2s_0s,opacity_0s_0.55s]"
+              "w-1.5 md:w-2 h-1.5 md:h-2 bg-stone-50 rounded-full inline-block origin-[-0.62rem_bottom] md:origin-[-0.55rem_bottom] xl:origin-[-0.45rem_bottom]",
+              "opacity-100 group-hover:opacity-0 group-hover:rotate-[115deg] md:group-hover:rotate-[134deg]",
+              "[transition:transform_0s,opacity_0.3s_0.55s] group-hover:[transition:transform_0.2s_0s,opacity_0s_0.55s]"
             )}
-            style={{
-              transition: "",
-            }}
           />
         </div>
       </h2>
