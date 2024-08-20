8c2a96e0c76c51a4fd0f4606706e6040bd5ce715
Cobelt <paul_emile@hotmail.fr>
2024-08-20 05:03:16 +0200

    feat: stack page base

--- src/pages/stack.tsx
+++ src/pages/stack.tsx
@@ -1,21 +1,181 @@
-import { IPageProps } from "@/types"
+import Button, { Colors } from "@/components/Button"
 import PageTitle from "@/components/PageTitle"
 import PageTransition from "@/components/PageTransition"
+import Title from "@/components/Title"
 import { cN } from "@/lib"
+import { IPageProps } from "@/types"
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+
+const CATEGORIES = ["knowHow", "techs", "softSkills"] as const
+const SKILLS = [
+  {
+    text: "Développer un site vitrine",
+    category: "knowHow",
+  },
+  {
+    text: "Développer un site marchand",
+    category: "knowHow",
+  },
+  {
+    text: "Développer un backoffice",
+    category: "knowHow",
+  },
+  {
+    text: "Développer une app interne",
+    category: "knowHow",
+  },
+  {
+    text: "Développer une app mobile",
+    category: "knowHow",
+  },
+  {
+    text: "Maintenir et faire évoluer un projet",
+    category: "knowHow",
+  },
+  {
+    text: "Maitriser la couche métier d'un projet",
+    category: "knowHow",
+  },
+  {
+    text: "JavaScript",
+    category: "techs",
+    subCategory: "languages",
+  },
+  {
+    text: "TypeScript",
+    category: "techs",
+    subCategory: "languages",
+  },
+  {
+    text: "PHP",
+    category: "techs",
+    subCategory: "languages",
+  },
+  {
+    text: "Ruby",
+    category: "techs",
+    subCategory: "languages",
+  },
+  {
+    text: "C++",
+    category: "techs",
+    subCategory: "languages",
+  },
+  {
+    text: "React",
+    category: "techs",
+    subCategory: "framework-front",
+  },
+  {
+    text: "Next",
+    category: "techs",
+    subCategory: "framework-front",
+  },
+  {
+    text: "Svelte",
+    category: "techs",
+    subCategory: "framework-front",
+  },
+  {
+    text: "Vue 2",
+    category: "techs",
+    subCategory: "framework-front",
+  },
+  {
+    text: "Symfony",
+    category: "techs",
+    subCategory: "framework-back",
+  },
+  {
+    text: "Ruby-On-Rails",
+    category: "techs",
+    subCategory: "framework-back",
+  },
+  {
+    text: "Silex",
+    category: "techs",
+    subCategory: "framework-back",
+  },
+  {
+    text: "Sharepoint",
+    category: "techs",
+    subCategory: "framework-back",
+  },
+  {
+    text: "Cypress",
+    category: "techs",
+    subCategory: "willing-to-learn",
+  },
+  {
+    text: "Dart + Flutter",
+    category: "techs",
+    subCategory: "willing-to-learn",
+  },
+  {
+    text: "Svelte (more)",
+    category: "techs",
+    subCategory: "willing-to-learn",
+  },
+  {
+    text: "Vue (more)",
+    category: "techs",
+    subCategory: "willing-to-learn",
+  },
+  {
+    text: "Travailler en équipe de tout taille",
+    category: "softSkills",
+  },
+  {
+    text: "Communiquer avec mes collègues",
+    category: "softSkills",
+  },
+  {
+    text: "M'adapter au besoin",
+    category: "softSkills",
+  },
+  {
+    text: "Prendre du recul",
+    category: "softSkills",
+  },
+  {
+    text: "Être créatif et inventif",
+    category: "softSkills",
+  },
+  {
+    text: "Être rigoureux et précis",
+    category: "softSkills",
+  },
+  {
+    text: "Être force de proposition",
+    category: "softSkills",
+  },
+  {
+    text: "Être versatile",
+    category: "softSkills",
+  },
+  {
+    text: "Être autonome",
+    category: "softSkills",
+  },
+]
 
 export default function Stack({ previousRoute }: IPageProps) {
+  const [t] = useTranslation()
+  const [tab, setTab] = useState("knowHow")
+
   return (
     <PageTransition
       headTitle="Mes compétences"
       previousRoute={previousRoute}
-      className="page:stack bg-blue-500 pt-8 px-8 pb-40 lg:pb-16 lg:pt-52 lg:px-[8vw] overflow-hidden"
+      className="page:stack bg-blue-500 overflow-hidden"
     >
       <main
         className={cN(
           "grid gap-x-6 md:gap-x-10 gap-y-4 lg:gap-y-8 2xl:gap-y-10",
-          "template-[base] 2xl:template-[2xl]",
-          "h-full overflow-x-hidden overflow-y-auto 2xl:overflow-y-hidden no-scrollbar",
-          "2xl:px-[5rem] 2xl:pb-24"
+          "template-[base] md:template-[md]",
+          "max-w-full h-full overflow-x-hidden overflow-y-auto xl:overflow-y-hidden no-scrollbar",
+          "px-8 lg:px-[8vw] 2xl:px-[5rem] 2xl:pb-24"
         )}
       >
         <PageTitle className="area-[pagetitle]">
@@ -24,63 +184,43 @@ export default function Stack({ previousRoute }: IPageProps) {
           </span>
         </PageTitle>
 
-        <section className="area-[know-how]">
-          <h2 className="font-archivo text-2xl">Concrètement</h2>
+        <div className="area-[tabs] flex flex-col items-stretch justify-center gap-4 md:gap-8 pt-8 pb-8 md:pt-16 md:pb-52">
+          {CATEGORIES.map((category) => (
+            <Button
+              key={category}
+              color={Colors.BLACK}
+              className={cN(
+                tab === category &&
+                  "!bg-stone-50 text-stone-800 hover:text-stone-800",
+                "px-6 py-4 md:px-16 md:py-6 text-xl"
+              )}
+              borderless
+              id={category}
+              onClick={() => setTab(category)}
+            >
+              {t("stack.category." + category)}
+            </Button>
+          ))}
+        </div>
 
-          <h4>Je sais ...</h4>
-          <ul className="font-rubikReg text-lg">
-            <li>Développer un site vitrine</li>
-            <li>Développer un site marchand</li>
-            <li>Développer un backoffice</li>
-            <li>Développer une app interne</li>
-            <li>Développer une app mobile</li>
-            <li>Maintenir et faire évoluer un projet</li>
-            <li>Maitriser la couche métier d{"'"}un projet existant</li>
-          </ul>
-        </section>
-        <section className="area-[techs]">
-          <h2 className="font-archivo text-2xl">Mes technos</h2>
+        <section className="area-[details] flex flex-col overflow-hidden md:p-16 border-4 border-dashed rounded-lg">
+          <Title.h3
+            className="px-6 md:px-8 py-4"
+            text={t("stack.category." + tab)}
+          />
 
-          <ul className="font-rubikReg text-lg">
-            <h4 className="text-xl">Languages</h4>
-            <li>JavaScript/TypeScript/EcmaScript 6+</li>
-            <li>PHP</li>
-            <li>Ruby</li>
-            <li>C++</li>
-            <li></li>
-            <h4 className="text-xl">Framework Front</h4>
-            <li>React</li>
-            <li>Next</li>
-            <li>Svelte</li>
-            <li>Vue 2</li>
-            <h4 className="text-xl">Framework Back</h4>
-            <li>Symfony</li>
-            <li>Ruby-On-Rails</li>
-            <li>Silex</li>
-            <li>Sharepoint</li>
-            <h4 className="text-xl">Willing to learn</h4>
-            <li>Cypress</li>
-            <li>Dart + Flutter</li>
-            <li>Svelte (more)</li>
-            <li>Vue (more)</li>
-          </ul>
-        </section>
-
-        <section className="area-[soft-skills]">
-          <h2 className="font-archivo text-2xl">Mes savoirs être</h2>
-
-          <h4>Je sais ...</h4>
-          <ul className="font-rubikReg text-lg">
-            <li>Travailler en équipe de tout taille</li>
-            <li>Communiquer avec mes collègues</li>
-            <li>M{"'"}adapter au besoin</li>
-            <li>Prendre du recul</li>
-            <li>Être créatif et inventif</li>
-            <li>Être rigoureux et précis</li>
-            <li>Être force de proposition</li>
-            <li>Être versatile</li>
-            <li>Être autonome</li>
-          </ul>
+          <div className="pt-4 pb-8 w-full h-full">
+            <div className="px-6 md:px-8 flex flex-wrap items-center gap-4 h-[calc(100%_-_3rem)] overflow-x-hidden">
+              {SKILLS.filter((skill) => skill.category === tab).map((skill) => (
+                <span
+                  key={skill.text}
+                  className="max-h-full bg-stone-50 rounded-full py-4 px-6 md:px-8 whitespace-nowrap overflow-hidden text-ellipsis"
+                >
+                  {skill.text}
+                </span>
+              ))}
+            </div>
+          </div>
         </section>
       </main>
     </PageTransition>
